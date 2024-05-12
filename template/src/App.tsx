101405707522a09a0315dcbad59e8e5ecf83a880
hidarikani <19387605+hidarikani@users.noreply.github.com>
2024-05-12 13:57:13 +0300

    Overlapping cells

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -5,13 +5,24 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="col-span-12 sm:col-span-10 md:col-span-10 lg:col-span-8 xl:col-span-6 2xl:col-span-4 bg-blue-500 ">
-      <span className="invisible sm:visible">SM</span>
-      <span className="invisible md:visible">MD</span>
-      <span className="invisible lg:visible">LG</span>
-      <span className="invisible xl:visible">XL</span>
-      <span className="invisible 2xl:visible">2XL</span>
-    </main>
+    <>
+      <header className="h-12 col-start-1 col-span-12 row-start-1 row-span-1 lg:col-span-4 bg-green-200">
+        <h1>Your App Name</h1>
+      </header>
+      <nav className="relative z-10 col-start-1 col-span-11 row-start-2 row-span-2 bg-orange-200">
+        <span>Nav</span>
+      </nav>
+      <main className="col-start-1 col-span-12 row-start-2 row-span-2 sm:col-span-10 md:col-span-10 lg:col-span-8 xl:col-span-6 2xl:col-span-4 bg-blue-200 ">
+        <span>Main</span>
+      </main>
+      <div className="fixed bottom-0 right-0 m-4 z-50 p-3 bg-blue-500 text-white rounded-lg shadow-lg">
+        <span className="invisible sm:visible">SM </span>
+        <span className="invisible md:visible">MD </span>
+        <span className="invisible lg:visible">LG </span>
+        <span className="invisible xl:visible">XL </span>
+        <span className="invisible 2xl:visible">2XL </span>
+      </div>
+    </>
 
     // <Grid
     //   areas={{
