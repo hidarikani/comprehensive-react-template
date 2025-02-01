865941c285a87268486fa89077ff2e9b0ae5e975
hidarikani <19387605+hidarikani@users.noreply.github.com>
2025-02-01 19:43:36 +0200

    Removed type awareness

--- template/eslint.config.mjs
+++ template/eslint.config.mjs
@@ -4,14 +4,12 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-// see https://stackoverflow.com/questions/72780296/warning-react-version-not-specified-in-eslint-plugin-react-settings-while-run
-
 export default [
   { files: ["**/*.{ts,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat['jsx-runtime'], // Add this if you are using React 17+
+  pluginReact.configs.flat['jsx-runtime'], // Removes the need for React import
   eslintConfigPrettier
 ];
